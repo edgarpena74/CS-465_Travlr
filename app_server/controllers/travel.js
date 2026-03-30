22edb37bd83b468443d27b7a66a5d16f2d922ac0
edgarpena74 <edgarignaciopena@gmail.com>
2026-03-29 23:39:27 -0400

    Module 3 baseline completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
+
+// Read trips data from the JSON file
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // Controller function for the travel page
 // Renders the travel view with a title
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  // Pass the trips data to the handlebars view
+  res.render('travel', { title: 'Travlr Getaways', trips});
 };
-
+/* 
+Please Note: It is not a best practice to read a JSON file every time the webserver processes a request. This is a method used during development to support rapid prototyping and should be replaced before the applications goes into production.
+*/
 // Export controller for route usage
 module.exports = {
   travel
